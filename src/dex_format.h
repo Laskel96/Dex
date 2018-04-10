ec9bd61ea917a5cef9201b2acfbd18508ea663f5
Laskel96 <tls09611@naver.com>
2018-04-09 22:32:34 -0700

    message

---
+++ src/dex_format.h
@@ -0,0 +1,30 @@
+#include<stdint.h>
+#include<unistd.h>
+#include<fcntl.h>
+typedef struct {
+  uint8_t magic[8]; // ubyte
+  uint32_t checksum; // uint
+  uint8_t signature[20];
+  uint32_t file_szie;
+  uint32_t header_size;
+  uint32_t endian_tag;
+  uint32_t link_size;
+  uint32_t link_off;
+  uint32_t map_off;
+  uint32_t string_ids_size;
+  uint32_t string_ids_off;
+  uint32_t type_ids_size;
+  uint32_t type_ids_off;
+  uint32_t proto_ids_size;
+  uint32_t proto_ids_off;
+  uint32_t field_ids_size;
+  uint32_t field_ids_off;
+  uint32_t method_ids_size;
+  uint32_t method_ids_off;
+  uint32_t class_defs_size;
+  uint32_t class_defs_off;
+  uint32_t data_size;
+  uint32_t data_off;
+} header_item;
+
+void dex_parser_header(int fd);
